479567231d31d27a47b95c8f0617dd38e7dbf00f
A01661436 <102175249+A01661436@users.noreply.github.com>
2022-03-23 10:50:23 -0600

    Se aplicaron los colores aleatorios para la comida y la víbora

--- snake.py
+++ snake.py
@@ -8,15 +8,17 @@ Exercises
 4. Change the snake to respond to mouse clicks.
 """
 
-from random import randrange
+import random
 from turtle import *
 
 from freegames import square, vector
 
+colors = ["blue", "green", "black", "yellow", "purple"]
+theColor1 = random.choice(colors)
+theColor2 = random.choice(colors)
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
-
 
 def change(x, y):
     """Change snake direction."""
@@ -43,17 +45,17 @@ def move():
 
     if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
+        food.x = random.randrange(-15, 15) * 10
+        food.y = random.randrange(-15, 15) * 10
     else:
         snake.pop(0)
 
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, theColor1)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, theColor2)
     update()
     ontimer(move, 100)
 
